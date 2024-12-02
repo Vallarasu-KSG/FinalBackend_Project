c09af9d47a68babecd42fef866b49d798189268f
Vallarasu K <kvallarasu2003@gmail.com>
2024-12-02 23:49:25 +0530

    Update server.js 2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/app",routerUrl);
 
-if(mongoose.connect('mongodb://127.0.0.1:27017/Student-management'))
+if(mongoose.connect('mongodb://localhost:27017/Student-management'))
 {
     console.log('Database is Connected')
 }
